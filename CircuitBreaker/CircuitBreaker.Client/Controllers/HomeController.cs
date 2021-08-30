106d2fa001a5516f62785bc9b081cafa85a29e67
pourya.montakhabii@yahoo.com <pourya.montakhabii@yahoo.com>
2021-08-30 23:03:47 +0430

    Add Pollicy library , configure retry policy , fallback and circuit breaker

--- CircuitBreaker/CircuitBreaker.Client/Controllers/HomeController.cs
+++ CircuitBreaker/CircuitBreaker.Client/Controllers/HomeController.cs
@@ -2,10 +2,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Polly;
+using Polly.CircuitBreaker;
+using Polly.Fallback;
+using Polly.Retry;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Threading.Tasks;
 
 namespace CircuitBreaker.Client.Controllers
@@ -14,13 +19,48 @@ namespace CircuitBreaker.Client.Controllers
     [Route("[controller]/[action]")]
     public class HomeController : ControllerBase
     {
-        private string _baseAddress = "https://localhost:44314/Home/Odd";
+        private string _baseAddress = "https://localhost:44314/Home/Ten";
+        private readonly AsyncRetryPolicy<HttpResponseMessage> _retryPolicy;
+        private readonly AsyncFallbackPolicy<HttpResponseMessage> _fallbackPolicy;
+        private static AsyncCircuitBreakerPolicy<HttpResponseMessage> _circuitBreakerPolicy =
+            Policy.HandleResult<HttpResponseMessage>(result => !result.IsSuccessStatusCode).Or<HttpRequestException>()
+            .CircuitBreakerAsync(2, TimeSpan.FromSeconds(10),
+                   (a, b) =>
+                   {
+                       string circuitLogMessage = "Break";
+                   },
+                    () =>
+                    {
+                        string circuitLogMessage = "Reset";
+                    }, () =>
+                    {
+                        string circuitLogMessage = "Half";
+                    }
+                );
+
+        public HomeController()
+        {
+            _retryPolicy = Policy.HandleResult<HttpResponseMessage>(result => !result.IsSuccessStatusCode)
+                .RetryAsync(5, (a, b) => { string s = "retry"; });
+
+            _fallbackPolicy = Policy.HandleResult<HttpResponseMessage>(result => !result.IsSuccessStatusCode).Or<BrokenCircuitException>()
+                .FallbackAsync(new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+                {
+                      Content = new ObjectContent(typeof(Message), new Message { 
+                        Id=100,
+                        Text="Try again a few second later"
+                      }, new JsonMediaTypeFormatter())
+                });
+        }
 
         [HttpGet]
         public async Task<IActionResult> Index()
         {
             var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(_baseAddress);
+            var response = await _fallbackPolicy.ExecuteAsync(
+                () => _retryPolicy.ExecuteAsync(
+                    () => _circuitBreakerPolicy.ExecuteAsync(
+                        () => httpClient.GetAsync(_baseAddress))));
             var stringResponse = await response.Content.ReadAsStringAsync();
             var deserializedResponse = JsonConvert.DeserializeObject<ClientMessage>(stringResponse);
             return Ok(deserializedResponse);
--- CircuitBreaker/CircuitBreaker.Server/Controllers/HomeController.cs
+++ CircuitBreaker/CircuitBreaker.Server/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -36,6 +37,53 @@ namespace CircuitBreaker.Server.Controllers
             {
                 Id = counter,
                 Text = "Request was returned bad request in even mode"
+            });
+        }
+
+        [HttpGet]
+        public IActionResult Ten()
+        {
+            counter += 1;
+            if (counter % 10 == 0)
+            {
+                return Ok(new Message
+                {
+                    Id = counter,
+                    Text = "Request was returned ok when the remainder of 10 is equal to 0"
+                });
+            }
+            return BadRequest(new Message
+            {
+                Id = counter,
+                Text = "Request was returned not ok when the remainder of 10 is not equal to 0"
+            });
+        }
+
+
+        [HttpGet]
+        public IActionResult Long()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            System.Threading.Thread.Sleep(20000);
+            stopwatch.Stop();
+
+            return Ok(new Message
+            {
+                Id = counter,
+                Text = $"Your request was responsed in {stopwatch.ElapsedMilliseconds / 1000} seconds."
+            });
+        }
+
+        [HttpGet]
+        public IActionResult Ex()
+        {
+            throw new Exception("The Exeption has occured...");
+            return Ok(new Message
+            {
+                Id=counter,
+                Text = "Request was Responsed OK"
             });
         }
     }
